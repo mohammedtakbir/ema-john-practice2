fa86af6f178df5e0727197f93a5b3b618702e21a
mohammedtakbir <takbirhossain678@gmail.com>
2022-10-08 00:21:33 +0600

    products style added

---
+++ src/components/Product/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ShoppingCartIcon } from '@heroicons/react/24/solid'
+const Product = ({ product }) => {
+    const { img, name, price, seller, ratings } = product;
+    return (
+        <div className='border p-4 rounded-lg relative'>
+            <img className='rounded-lg' src={img} alt="" />
+            <div className='mt-5 mb-7'>
+                <h2 className='text-xl font-semibold mb-1'>{name}</h2>
+                <h3 className='font-medium'>Price: ${price}</h3>
+            </div>
+            <div className='pb-5'>
+                <p className='text-[#2A414F] text-sm mb-2'>Manufacturer: {seller}</p>
+                <p className='text-[#2A414F] text-sm'>Ratings: {ratings} star</p>
+            </div>
+            <div className='flex justify-center mt-10'>
+                <button className='font-medium bg-[#FFE0B3] w-full absolute bottom-0 py-3 hover:bg-[orange] text-xl'>
+                    <span className='mr-3'>Add to Cart</span>
+                    <ShoppingCartIcon className="h-6 w-6 text-black inline"/>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-
+import Product from '../Product/Product';
+import './Shop.css'
 const Shop = () => {
     const products = useLoaderData()
-    console.log(products)
     return (
-        <div>
-            <h1>total Products: {products?.length}</h1>
-        </div>
+        <>
+            <div className="shop-container container mx-auto custom-grid1">
+                <div className="products-container custom-grid2 mt-16">
+                    {
+                        products.map(product => <Product 
+                            key={product.id}
+                            product={product}
+                            />)
+                    }
+                </div>
+                <div className="cart-container">
+                    <h2>cart summery</h2>
+                </div>
+            </div>
+        </>
     );
 };
 
