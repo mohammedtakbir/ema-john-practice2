0a993bc9c67066cca028228494e2990cde70ae9d
mohammedtakbir <takbirhossain678@gmail.com>
2022-10-18 14:37:05 +0600

    implement onAuthStateChanged and user signout

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,13 @@ import { Link, NavLink } from 'react-router-dom';
 import { AuthContext } from '../../contexts/UserContext';
 import logo from '../../images/Logo.svg'
 const Navbar = () => {
-    const {user} = useContext(AuthContext);
+    const { user, userSignOut } = useContext(AuthContext);
+    //* Sign Out
+    const handleSignOut = () => {
+        userSignOut()
+            .then(() => { })
+            .catch(err => console.error(err))
+    }
     return (
         <div className='bg-[#1C2B35] py-4'>
             <div className='flex container mx-auto justify-between items-center md:px-0 px-3'>
@@ -12,21 +18,43 @@ const Navbar = () => {
                     <img src={logo} alt="" />
                 </Link>
                 <div className=''>
-                    <NavLink style={({isActive}) => {
-                        return isActive ? {color: 'orange'} : undefined
+                    <NavLink style={({ isActive }) => {
+                        return isActive ? { color: 'orange' } : undefined
                     }} className='no-underline ml-10 text-white md:text-lg hover:text-orange-300' to='/shop'>Shop</NavLink>
-                    <NavLink style={({isActive}) => {
-                        return isActive ? {color: 'orange'} : undefined
+                    <NavLink style={({ isActive }) => {
+                        return isActive ? { color: 'orange' } : undefined
                     }} className='no-underline ml-10 text-white md:text-lg hover:text-orange-300' to='/orders'>Orders</NavLink>
-                    <NavLink style={({isActive}) => {
-                        return isActive ? {color: 'orange'} : undefined
+                    <NavLink style={({ isActive }) => {
+                        return isActive ? { color: 'orange' } : undefined
                     }} className='no-underline ml-10 text-white md:text-lg hover:text-orange-300' to='/inventory'>Inventory</NavLink>
-                    <NavLink style={({isActive}) => {
-                        return isActive ? {color: 'orange'} : undefined
-                    }} className='no-underline ml-10 text-white md:text-lg hover:text-orange-300' to='/login'>Login</NavLink>
-                    <NavLink style={({isActive}) => {
-                        return isActive ? {color: 'orange'} : undefined
-                    }} className='no-underline ml-10 text-white md:text-lg hover:text-orange-300' to='/signup'>Sign Up</NavLink>
+
+                    {user?.email ?
+                        <NavLink onClick={handleSignOut} className='no-underline ml-10 text-white md:text-lg hover:text-orange-300'>Sign Out</NavLink> :
+                        <>
+                            <NavLink style={({ isActive }) => {
+                                return isActive ? { color: 'orange' } : undefined
+                            }} className='no-underline ml-10 text-white md:text-lg hover:text-orange-300' to='/login'>Login</NavLink>
+                            <NavLink style={({ isActive }) => {
+                                return isActive ? { color: 'orange' } : undefined
+                            }} className='no-underline ml-10 text-white md:text-lg hover:text-orange-300' to='/signup'>Sign Up</NavLink>
+                        </>
+                    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                     <span className='text-white ml-3'>{user?.email}</span>
                 </div>
             </div>
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,30 +1,43 @@
 import React from 'react';
-import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'firebase/auth';
 import app from '../firebase/firebase.config';
 import { createContext } from 'react';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const UserContext = ({children}) => {
-    // const [user, setUser] = useState(null);
-    const user = {email: 'takbir@123gmail.com'}
+    const [user, setUser] = useState(null);
 
     //* Sign Up an user 
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
-    }
+    };
 
     //* Login an user
     const userLogIn = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password);
-    }
+    };
+
+    //*get the currently sign-in user
+    useEffect(() => {
+        const unsubscribe=  onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+        })
+        return () => unsubscribe();
+    }, []);
+
+    //* Sing Out
+    const userSignOut = () => {
+        return signOut(auth);
+    };
 
 
 
 
-    const authInfo = {user, createUser, userLogIn}
+    const authInfo = {user, createUser, userLogIn, userSignOut}
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
